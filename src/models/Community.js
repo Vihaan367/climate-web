a0bde4254eba314fbcdec8a38d2733c42e1f8163
Vihaan367 <e10989vihaanpro@gmail.com>
2026-08-03 01:07:32 +0530

    Updated Community.js

--- src/models/Community.js
+++ src/models/Community.js
@@ -2,39 +2,34 @@ const mongoose = require("mongoose");
 
 const communitySchema = new mongoose.Schema(
 {
-name: {
-type: String,
-required: true
+    name: {
+        type: String,
+        required: true
     },
 
-location: {
-type: String,
-required: true
+    location: {
+        type: String,
+        required: true
     },
 
-riskLevel: {
-type: String,
-enum: ["Low", "Medium", "High"],
-default: "Low"
+    poolBalance: {
+    type: Number,
+    default: 0
     },
 
-totalMembers: {
-type: Number,
-default: 0
+    riskLevel: {
+        type: String,
+        enum: ["Low", "Medium", "High"],
+        default: "Low"
     },
 
-threshold: {
-type: Number,
-default: 250
-    },
-
-poolBalance: {
-type: Number,
-default: 0
+    totalMembers: {
+        type: Number,
+        default: 0
     }
 },
 {
-timestamps: true
+    timestamps: true
 });
 
 module.exports = mongoose.model("Community", communitySchema);
